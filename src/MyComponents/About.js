1e30b782dda554f81cb91dab55bdbba015995a2c
Manmohan Sisodiya <manmohansisodiya2299@gmail.com>
2021-10-04 17:07:28 +0530

    my project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an about componenet
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore dicta ipsa, repellat amet ea cum velit iure error repellendus dolore aut ratione quasi laboriosam eligendi sequi fugit vero accusantium dolorum.
+            </p>
+        </div>
+    )
+}
